e6b4ebb5aafa7e73248572529bd3918ea375309f
MD Murad Hossain <mdmuradhossainmiraj@gmail.com>
2022-12-06 20:13:08 +0600

    update media player

--- CustomVideoPlayer/CustomVideoPlayer/Controller/ViewController.swift
+++ CustomVideoPlayer/CustomVideoPlayer/Controller/ViewController.swift
@@ -12,17 +12,25 @@ import AVKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var videoViewP: UIView!
-    
     @IBOutlet weak var plusButton: UIButton!
     
     var avPlayer: AVPlayer!
     var avpController = AVPlayerViewController()
+    
+    var selectedVideoUrl: URL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
+    func playVideo(_ url: URL){
+        avPlayer = AVPlayer(url: url)
+        avpController.player = avPlayer
+        avpController.view.frame.size.height = videoViewP.frame.size.height
+        avpController.view.frame.size.width = videoViewP.frame.size.width
+        self.videoViewP.addSubview(avpController.view)
+    }
     
     @IBAction func videoAddButtion(_ sender: Any) {
         let pickerController = UIImagePickerController()
@@ -40,12 +48,7 @@ extension ViewController:  UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true, completion: nil)
         guard let movieUrl = info[.mediaURL] as? URL else { return }
-        avPlayer = AVPlayer(url: movieUrl)
-        avpController.player = avPlayer
-        avpController.view.frame.size.height = videoViewP.frame.size.height
-        avpController.view.frame.size.width = videoViewP.frame.size.width
-        self.videoViewP.addSubview(avpController.view)
-        avPlayer.play()
+        playVideo(movieUrl)
     }
 }
 
--- CustomVideoPlayer/CustomVideoPlayer/System/AppDelegate.swift
+++ CustomVideoPlayer/CustomVideoPlayer/System/AppDelegate.swift
@@ -9,15 +9,12 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-var window: UIWindow?
     
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        // Override point for customization after application launch.
         return true
     }
-
-  
 }
 
